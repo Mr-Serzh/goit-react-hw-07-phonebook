2d0d7398d72f159bf6f89db12b16b4a17f5fa356
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2021-04-17 22:51:46 +0300

    fixed background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Container from './components/Container/Container';
 import Form from './components/Form/Form';
 import ContactForm from './components/ContactForm/ContactForm';
 import Filter from './components/Filter/Filter';
@@ -8,15 +7,13 @@ import ContactList from './components/ContactList/ContactList';
 
 export default function App() {
   return (
-    <Container>
-      <Form>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-        <ToastContainer autoClose={2700} />
-      </Form>
-    </Container>
+    <Form>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+      <ToastContainer autoClose={2700} />
+    </Form>
   );
 }
